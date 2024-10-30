e71d1adfbd91eeb832eb16380ecee8e81c2707b9
noormmd <noormamdouh004@gmail.com>
2024-10-30 10:58:44 +0000

    Initial Vue.js setup with basic lesson and cart functionality

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="en">
+   <div id="app">
 
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <header>
+      <!-- Website name -->
+      <h1>{{ LessonsStore }}</h1>
+    </header>
+
     <!-- <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script> -->
     <link href='https://fonts.googleapis.com/css?family=Fraunces' rel='stylesheet'>
     <title>Coursework for Full Stack</title>
-  </head>
-
-  <body>
-    <div id="app">
+ 
       <!--
       <header>
         To add header using vue.js
@@ -20,9 +17,9 @@
       </header>
     -->
     </div>
-  </body>
+  
   <main>
-    <img alt="Cart" id="cart" src="../public/cart-icon.png">
+    <a id="cart"><img alt="Cart" id="carticon" src="../public/cart-icon.png"></a>
     <!-- Search functionality via to do list workshop-->
     <div id="SearchFunctionality">
       <h1>Lessons</h1>
@@ -45,30 +42,68 @@
       <div id="Lesson3">
         <h3>Maths</h3>
         <p>Information: </p>
-        <button class="addToCartButton">Add to cart</button>
+        <button class="addToCartButton" @:onclick="addItem">Add to cart</button>
       </div>
 
     </div>
-
-<!--
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
--->
   </main>
 
-  </html>
+
 </template>
 
 <script>
-/*
-import HelloWorld from './components/HelloWorld.vue'
+//import { ref } from 'vue';
 
-export default {
+    export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data() {
+    return {
+      sitename: "Lessons Website",
+      lessons: [
+        { id: 1001, title: "Geography", description: "Geography lessons" },
+        { id: 1002, title: "English", description: "English lessons" },
+        { id: 1003, title: "Maths", description: "Math lessons" }
+      ],
+      cart: []
+    };
+  },
+    
+  cart: {
+    0: 1001,
+    1: 1002,
+    2: 1003,
+    3: 1004
+  },
+
+  methods: {
+    addItem: function () {
+      this.cart.push(this.product.id)
+
 }
+  }
+};
+
+
+/** 
+// Functionality to show specific sections after hiding the mainPage section
+  // To hide the mainPage section that's visible by default
+  function hideMainPage() {
+    document.getElementById('mainPage').style.display = 'none';
+  }
+
+  // To show the targeted section and hide the mainPage section once another section is selected
+  function showSection(sectionId) {
+    // Calling function that hides the mainPage section
+    hideMainPage();
+    // Updates the hash in the URL to show the targeted section
+    window.location.hash = sectionId.substring(1);
+  }
+  */
+
+/*
+
 */
 </script>
 
@@ -81,9 +116,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  font-family: 'Fraunces';font-size: 22px;
+  font-family: 'Fraunces';
+  font-size: 22px;
   /**font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;*/
- margin: -8px; /** To make website fit page */
+  margin: -8px;
+  /** To make website fit page */
 }
 
 p {
@@ -102,7 +139,9 @@ p {
 }
 
 /*Each individual div of the lesson items*/
-#Lesson1, #Lesson2, #Lesson3{
+#Lesson1,
+#Lesson2,
+#Lesson3 {
   border: solid 2px;
   margin-top: 10px;
   margin-bottom: 10px;
@@ -111,48 +150,49 @@ p {
   padding-bottom: 20px;
   flex: content;
   align-items: flex-start;
-  border-style:inset;
+  border-style: inset;
   padding-right: 80%;
   padding-left: 20px;
 }
 
 /*When hovering over lesson item, change background*/
-#Lesson1:hover{
+#Lesson1:hover {
   background-image: url(../public/geography.jpg);
   color: white;
 }
 
-#Lesson2:hover{
+#Lesson2:hover {
   background-image: url(../public/english.jpg);
   color: white;
 }
 
-#Lesson3:hover{
+#Lesson3:hover {
   background-image: url(../public/maths.jpg);
   color: white;
 }
 
 /*Search bar input form*/
-  #searchBar {
-    color: black;
-border-radius: 30px;
-border: 1px solid;
-padding: 10px;
-width: 250px;
-margin-left: 16%;
+#searchBar {
+  color: black;
+  border-radius: 30px;
+  border: 1px solid;
+  padding: 10px;
+  width: 250px;
+  margin-left: 16%;
   align-items: center;
-    justify-content: center;
+  justify-content: center;
 }
 
 /*Buttons styling*/
-#searchButton, .addToCartButton {
-border-radius: 30px;
-border: 1px solid;
-padding: 10px;
+#searchButton,
+.addToCartButton {
+  border-radius: 30px;
+  border: 1px solid;
+  padding: 10px;
   align-items: center;
-    justify-content: center;
-    margin-left: 10px;
-    text-decoration-color: azure;
+  justify-content: center;
+  margin-left: 10px;
+  text-decoration-color: azure;
 
   appearance: none;
   background-color: transparent;
@@ -162,13 +202,13 @@ padding: 10px;
   color: #3B3B3B;
   cursor: pointer;
   display: inline-block;
-  font-family: Roobert,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+  font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   font-size: 16px;
   font-weight: 600;
   line-height: normal;
   margin: 0;
-  height:50px;
-  width:190px;
+  height: 50px;
+  width: 190px;
   outline: none;
   padding: 12px 10px;
   text-align: center;
@@ -181,12 +221,14 @@ padding: 10px;
   margin-left: 15px;
 }
 
-#searchButton:disabled, .addToCartButton:disabled {
+#searchButton:disabled,
+.addToCartButton:disabled {
   pointer-events: none;
 }
 
 /*Change style and background colour when hovering*/
-#searchButton:hover, .addToCartButton:hover {
+#searchButton:hover,
+.addToCartButton:hover {
   color: #fff;
   background-color: #1A1A1A;
   box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
@@ -199,19 +241,20 @@ padding: 10px;
 }
 
 /*Cart icon*/
-#cart {
+#carticon {
   margin-left: 70%;
   margin-top: 10px;
+  margin-bottom: -30px;
 }
 
 /*Styling of add to cart button*/
 .addToCartButton {
-border-radius: 15px;
-border: solid 1px;
+  border-radius: 15px;
+  border: solid 1px;
 }
 
 /*Change colour of button when hovering over it*/
 button:hover {
-background-color: darkgray;
+  background-color: darkgray;
 }
 </style>
